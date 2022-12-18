5c5777920676282d5448369b9f02269d90bdf14c
kadiryaparfinland <kadiryapar16@gmail.com>
2022-12-18 15:46:52 +0200

    first commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  template: `
+  <section>
+      <h1>I'm in the Servers Component</h1>
+      <p>Below this I have 2 server components</p>
+      <app-server></app-server>
+      <app-server></app-server>
+  </section>
+  `,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
