34476c7e5f48bf069052a0c1b49f28e52799f0c8
kadiryaparfinland <kadiryapar16@gmail.com>
2022-12-18 23:02:54 +0200

    first commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <section>
-      <h1>I'm in the Servers Component</h1>
-      <p>Below this I have 2 server components</p>
-      <app-server></app-server>
-      <app-server></app-server>
-  </section>
-  `,
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
 
 }
